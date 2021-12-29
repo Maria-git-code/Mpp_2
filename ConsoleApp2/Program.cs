b6d96bb2f929a049163fe4c813d38dc23f8885a6
Maria-git-code <83037470+Maria-git-code@users.noreply.github.com>
2021-12-29 15:43:35 +0300

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            if (args.Length != 2)
+            {
+                Console.Out.WriteLine("Incorrect number of arguments.");
+            }
+            else
+            {
+                var src = args[0];
+                var dest = args[1];
+                FileCopier copier = new FileCopier();
+                Console.Out.WriteLine("Copied files " + copier.Copy(src, dest));
+            }
+
+        }
+
+        
+    }
+}
